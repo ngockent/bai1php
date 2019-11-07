642a819dc60cdfa4cba68ebd595175ea4ff6d9a9
ngockent <bangsh96lc@gmail.com>
2019-11-08 01:29:26 +0700

    update 1

--- bai1.php
+++ bai1.php
@@ -1,54 +1,57 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>bai1</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
 	<?php
-		include 'connect.php';
-	?>	
-	<form method="post">				
+	include 'connect.php';
+	include 'function.php';
+	// var_dump('<pre>');
+	// var_dump(abc($con));
+	// var_dump('</pre>');
+	die;
+	?>
+	<form method="post">
 		<div class="container">
 			<div class="row-1">
-				<h1 name="id">Đặt vé máy bay</h1>			
+				<h1 name="id">Đặt vé máy bay</h1>
 			</div>
-			<hr>			
+			<hr>
 			<div class="row" style="color: #000;">
 				<div class="col-md-6">
-					<label class="col-md-6">Điểm đi</label>					
+					<label class="col-md-6">Điểm đi</label>
 					<select name="diemdi" class="col-md-6">
-					<?php
-						$sql = "select * from diadiem";
-						$exe = mysqli_query($con,$sql);
-						while ($num = mysqli_fetch_array($exe)) {
-					?>
-						<option><?php echo $num['DiemDi'];?></option><br>
-					<?php }?>
-					</select>					
-						<br>			
-					<lable class="col">Ngày đi</lable><br>
-					<input type="text" name="ngaydi" class="col-md-2"  min="1" max="31" step="1" placeholder="1">
-					<input type="month" class="col-md-7" name="thangdi">
-				</div>	
-				<div class="col-md-6">
-					<label class="col">Điểm đến</label>					
-					<select name="diemden" value="col-md-9">
-					<?php
-						$sql = "select * from diadiem";
-						$exe = mysqli_query($con,$sql);
-						while ($num = mysqli_fetch_array($exe)) {
-					?>
-						<option><?php echo $num['DiemDen'];?></option>
-					<?php }?>
+						<?php
+						foreach (abc($con) as $key => $value) {
+							?>
+							<option><?php echo $value["diemdi"] ?></option><br>
+						<?php } ?>
 					</select>
-					<br>		
-					<label class="col-md-6">Ngày về</label><br>	
+					<br>
+					<lable class="col">Ngày đi</lable><br>
+					<input type="text" name="ngaydi" class="col-md-2" min="1" max="31" step="1" placeholder="1">
+					<input type="month" class="col-md-7" name="thangdi">
+				</div>
+				<div class="col-md-6">
+					<label class="col">Điểm đến</label>
+					<select name="diemden" value="col-md-9">
+						<?php
+						foreach (abc($con) as $key => $value) {
+							?>
+							<option><?php echo $value['diemden']; ?></option>
+						<?php } ?>
+					</select>
+					<br>
+					<label class="col-md-6">Ngày về</label><br>
 					<input type="text" class="col-md-2" name="ngayve" min="1" max="31" step="1" placeholder="1">
 					<input type="month" class="col-md-7" name="thangve">
-				</div>		
+				</div>
 			</div>
 			<hr>
 			<div class="row">
@@ -69,28 +72,28 @@
 			</div>
 		</div>
 		<?php
-			if(isset($_POST['OK']))
-			{
-				$diemdi = $_POST['diemdi'];
-				$diemden = $_POST['diemden'];
-				$ngaydi = $_POST['ngaydi'];
-				$thangdi = date("M,Y", strtotime($_POST['thangdi']));
-				$ngayve = $_POST['ngayve'];
-				$thangve = date("M,Y",strtotime($_POST['thangve']));
-				$nguoilon = $_POST['nguoilon'];
-				$treem = $_POST['treem'];
-				$embe = $_POST['embe'];
-				$total = 0;
-				
-				$dayin = $ngaydi.','.$thangdi;
-				$dayout = $ngayve.','.$thangve;
-				$total = 2000000*(int)($nguoilon) + 2000000*25/100*(int)($treem) + 2000000*80/100*($embe);
+		if (isset($_POST['OK'])) {
+			$diemdi = $_POST['diemdi'];
+			$diemden = $_POST['diemden'];
+			$ngaydi = $_POST['ngaydi'];
+			$thangdi = date("M,Y", strtotime($_POST['thangdi']));
+			$ngayve = $_POST['ngayve'];
+			$thangve = date("M,Y", strtotime($_POST['thangve']));
+			$nguoilon = $_POST['nguoilon'];
+			$treem = $_POST['treem'];
+			$embe = $_POST['embe'];
+			$total = 0;
 
-				$sqlselect = "Insert into datve(DiemDi,DiemDen,NgayDi,NgayVe,NguoiLon,TreEm,EmBe,TotalMoney)
+			$dayin = $ngaydi . ',' . $thangdi;
+			$dayout = $ngayve . ',' . $thangve;
+			$total = 2000000 * (int) ($nguoilon) + 2000000 * 25 / 100 * (int) ($treem) + 2000000 * 80 / 100 * ($embe);
+
+			$sqlselect = "Insert into datve(DiemDi,DiemDen,NgayDi,NgayVe,NguoiLon,TreEm,EmBe,TotalMoney)
 					values('$diemdi','$diemden','$dayin','$dayout','$nguoilon','$treem','$embe','$total')";
-				$exeInsert = mysqli_query($con,$sqlselect);				
-			}
+			$exeInsert = mysqli_query($con, $sqlselect);
+		}
 		?>
 	</form>
 </body>
+
 </html>
